d4d329219d323574ff4fda803d63c759d206bdf5
alanzarate <alnzarate@gmail.com>
2023-04-21 07:47:27 -0400

    initial commit

---
+++ src/main/kotlin/com/ucb/bo/sktmsdiscovery/SktMsDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package com.ucb.bo.sktmsdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SktMsDiscoveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SktMsDiscoveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/ucb/bo/sktmsdiscovery/SktMsDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ucb.bo.sktmsdiscovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SktMsDiscoveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
