1e9f1e451fe4096209157fdad7c573c549b1a726
EmersonChipana <emerson.chipana@ucb.edu.bo>
2023-04-23 13:00:39 -0400

    Agregando config server

--- src/main/kotlin/com/ucb/bo/sktmsdiscovery/SktMsDiscoveryApplication.kt
+++ src/main/kotlin/com/ucb/bo/sktmsdiscovery/SktMsDiscoveryApplication.kt
@@ -2,8 +2,10 @@ package com.ucb.bo.sktmsdiscovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SktMsDiscoveryApplication
 
 fun main(args: Array<String>) {
